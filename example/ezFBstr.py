86027be0ecfc3a102c314a5d7406303fb7d3e366
Owen <owen.carter@gmail.com>
2024-03-20 02:12:24 +0100

    badly not working

--- example/ezFBstr.py
+++ example/ezFBstr.py
@@ -10,40 +10,28 @@ import framebuf
 from uctypes import bytearray_at, addressof
 from sys import implementation
 
-def _get_id(device):
-    if not isinstance(device, framebuf.FrameBuffer):
-        raise ValueError('Device must be derived from FrameBuffer.')
-    return id(device)
+# a table to find max color and data width
+colorspaces = {
+                framebuf.MONO_VLSB : 2,
+                framebuf.MONO_HLSB : 2,
+                framebuf.MONO_HMSB : 2,
+                framebuf.RGB565 : 65536,
+                framebuf.GS2_HMSB: 4,
+                framebuf.GS4_HMSB: 16,
+                framebuf.GS8 : 256,
+                }
 
-# Basic Writer class for monochrome displays
-class Writer():
+# Basic string printing class
+class ezFBstr():
 
-    state = {}  # Holds a display state for each device
+    def __init__(self, device, font, fmt=framebuf.MONO_VLSB, color=None, halign='left', valign='top', rot=0, verbose=True):
+        self._device = device
+        self._font = font
+        self._fmt = fmt
 
-    @staticmethod
-    def set_textpos(device, row=None, col=None):
-        devid = _get_id(device)
-        if devid not in Writer.state:
-            Writer.state[devid] = DisplayState()
-        s = Writer.state[devid]  # Current state
-        if row is not None:
-            if row < 0 or row >= device.height:
-                raise ValueError('row is out of range')
-            s.text_row = row
-        if col is not None:
-            if col < 0 or col >= device.width:
-                raise ValueError('col is out of range')
-            s.text_col = col
-        return s.text_row,  s.text_col
-
-    def __init__(self, device, font, verbose=True):
-        self.devid = _get_id(device)
-        self.device = device
-        if self.devid not in Writer.state:
-            Writer.state[self.devid] = DisplayState()
-        self.font = font
         if font.height() >= device.height or font.max_width() >= device.width:
             raise ValueError('Font too large for screen')
+
         # Allow to work with reverse or normal font mapping
         if font.hmap():
             self.map = framebuf.MONO_HMSB if font.reverse() else framebuf.MONO_HLSB
@@ -52,120 +40,79 @@ class Writer():
         if verbose:
             fstr = 'Orientation: Horizontal. Reversal: {}. Width: {}. Height: {}.'
             print(fstr.format(font.reverse(), device.width, device.height))
-            print('Start row = {} col = {}'.format(self._getstate().text_row, self._getstate().text_col))
-        self.screenwidth = device.width  # In pixels
-        self.screenheight = device.height
-        self.bgcolor = 0  # Monochrome background and foreground colors
-        self.fgcolor = 1
-        self.cpos = 0
 
-        self.glyph = None  # Current char
-        self.char_height = 0
-        self.char_width = 0
-        self.clip_width = 0
+        self._screenwidth = device.width  # In pixels
+        self._screenheight = device.height
 
-    def _getstate(self):
-        return Writer.state[self.devid]
+        self._colormax = colorspaces[fmt] -1
 
-    def set_clip(self, row_clip=None, col_clip=None, wrap=None):
-        if row_clip is not None:
-            self.row_clip = row_clip
-        if col_clip is not None:
-            self.col_clip = col_clip
-        if wrap is not None:
-            self.wrap = wrap
-        return self.row_clip, self.col_clip, self.wrap
+        # Need to test color range and transparency key
+        # put in a local func()
+        self._fg = color[0]
+        self._bg = color[1]
+        self._tkey = color[2]
 
-    @property
-    def height(self):  # Property for consistency with device
-        return self.font.height()
+        # Alignment also needs testing
+        # put in a local func()
+        self._halign = _check_halign(halign)
+        self._valign = _check_valign(valign)
+        self._rot = rot
 
-    def printstring(self, string, invert=False):
-        # word wrapping. Assumes words separated by single space.
-        q = string.split('\n')
-        last = len(q) - 1
-        for n, s in enumerate(q):
-            if s:
-                self._printline(s, invert)
-            if n != last:
-                self._printchar('\n')
+    def _check_color(self, color):
+        ret = (self._colormax, 0, -1)
+        if color is None:
+            return ret
+        if len(color) not in (1,2,3):
+            raise ValueError('Unknown color tuple: ' + str(color))
+        ret[0] = self._color_range(color[0])
+        if len(color) > 1:
+            ret[1] = self._color_range(color[1])
+        else:
+            ret[1] = 0
+        if len(color) > 2:
+            if color[2] != -1:
+                ret[2] = self._color_range(color[2])
+            else:
+                ret[2] = -1
+        return ret
 
-    def _printline(self, string, invert):
-        rstr = None
-        if self.wrap and self.stringlen(string, True):  # Length > self.screenwidth
-            pos = 0
-            lstr = string[:]
-            while self.stringlen(lstr, True):  # Length > self.screenwidth
-                pos = lstr.rfind(' ')
-                lstr = lstr[:pos].rstrip()
-            if pos > 0:
-                rstr = string[pos + 1:]
-                string = lstr
+    def _color_range(self, color):
+        if color < 0:
+            color = 0
+        if color > self._colormax:
+            color = self._colormax
+        return color
 
-        for char in string:
-            self._printchar(char, invert)
-        if rstr is not None:
-            self._printchar('\n')
-            self._printline(rstr, invert)  # Recurse
+    def _check_halign(self, halign):
+        if halign not in ('left','center','right'):
+            raise ValueError('Unknown horizontal alignment: ' + halign)
+        if verbose:
+            print('horizontal alignment: ' + halign)
+        return halign
 
-    def stringlen(self, string, oh=False):
+    def _check_valign(self, valign):
+        if valign not in ('top','center','baseline','bottom'):
+            raise ValueError('Unknown vertical alignment: ' + valign)
+        if verbose:
+            print('vertical alignment: ' + valign)
+        return valign
+
+    def size(self, string):
         if not len(string):
-            return 0
-        sc = self._getstate().text_col  # Start column
-        wd = self.screenwidth
-        l = 0
+            return (0,0)
+        x = 0
         for char in string[:-1]:
             _, _, char_width = self.font.get_ch(char)
-            l += char_width
-            if oh and l + sc > wd:
-                return True  # All done. Save time.
-        char = string[-1]
-        _, _, char_width = self.font.get_ch(char)
-        if oh and l + sc + char_width > wd:
-            l += self._truelen(char)  # Last char might have blank cols on RHS
-        else:
-            l += char_width  # Public method. Return same value as old code.
-        return l + sc > wd if oh else l
+            x += char_width
+        return (x,self._font.height())
 
-    # Return the printable width of a glyph less any blank columns on RHS
-    def _truelen(self, char):
-        glyph, ht, wd = self.font.get_ch(char)
-        div, mod = divmod(wd, 8)
-        gbytes = div + 1 if mod else div  # No. of bytes per row of glyph
-        mc = 0  # Max non-blank column
-        data = glyph[(wd - 1) // 8]  # Last byte of row 0
-        for row in range(ht):  # Glyph row
-            for col in range(wd -1, -1, -1):  # Glyph column
-                gbyte, gbit = divmod(col, 8)
-                if gbit == 0:  # Next glyph byte
-                    data = glyph[row * gbytes + gbyte]
-                if col <= mc:
-                    break
-                if data & (1 << (7 - gbit)):  # Pixel is lit (1)
-                    mc = col  # Eventually gives rightmost lit pixel
-                    break
-            if mc + 1 == wd:
-                break  # All done: no trailing space
-        # print('Truelen', char, wd, mc + 1)  # TEST 
-        return mc + 1
+    def area(self, string, (x,y), halign='left', valign='top'):
+        # tbd
+        xmin = xmax = ymin = ymax = 0
+        return (xmin,xmax,ymin,ymax)
 
-    def _get_char(self, char, recurse):
-        self.glyph = None  # Assume all done
+    def _put_char(self, char, recurse):
         glyph, char_height, char_width = self.font.get_ch(char)
-        s = self._getstate()
-        if s.text_row + char_height > self.screenheight:
-            if self.row_clip:
-                return
-        oh = s.text_col + char_width - self.screenwidth  # Overhang (+ve)
-        if oh > 0:
-            if self.col_clip or self.wrap:
-                np = char_width - oh  # No. of printable columns
-                if np <= 0:
-                    return   # OWEN: - ?return False: hit limit
-        self.glyph = glyph
-        self.char_height = char_height
-        self.char_width = char_width
-        self.clip_width = char_width if np is None else np
 
     # Method using blitting. Efficient rendering for monochrome displays.
     # Tested on SSD1306. Invert is for black-on-white rendering.
@@ -183,5 +130,13 @@ class Writer():
         s.text_col += self.char_width
         self.cpos += 1
 
-    def setcolor(self, *_):
-        return self.fgcolor, self.bgcolor
+    @property
+    def color(self):
+        return(self._fg, self._bg, self._tkey)
+
+    @color.setter
+    def color(self, fg=self._fg, bg=self._bg, tkey=-1):
+        # check in range
+        self._fg = fg
+        self._bg = bg
+        self._tkey = tkey
