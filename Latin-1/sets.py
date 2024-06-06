390980935c09936b1f29015441d6b7f69d02a7d9
Owen Carter <owen.carter@gmail.com>
2024-06-06 13:28:10 +0200

    make this return lists not bytearrays

--- Latin-1/sets.py
+++ Latin-1/sets.py
@@ -20,11 +20,11 @@ fonts = [
         ]
 
 charsets = {
-        'time':list([32, 43, 45, 46] + range(48, 59)),
-        'num':list([32, 37] + range(40, 59) + [176]),
+        'time':list([32, 43, 45, 46] + list(range(48, 59))),
+        'num':list([32, 37] + list(range(40, 59)) + [176]),
         'upper':list(range(32, 96)),
         'ascii':list(range(32, 127)),
         'supp':list(range(160, 256)),
-        'latin':list(range(32, 127) + range(160, 256)),
+        'latin':list(list(range(32, 127)) + list(range(160, 256))),
         'full':list(range(0, 256))
         }
