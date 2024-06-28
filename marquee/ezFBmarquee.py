8aa883c9155ea59916506837b5e3a0064d6f9975
Owen <owen@easytarget.org>
2024-06-28 17:37:24 +0200

    working even better

--- marquee/ezFBmarquee.py
+++ marquee/ezFBmarquee.py
@@ -26,43 +26,46 @@ class ezFBmarquee():
         self._string = string.split('\n')[0]
         self._y = y
         self._seperation = max(seperation,0)
-        self._pause = max(pause,0)
         self._fg = fg
         self._bg = bg
         self._hgap = hgap
         self._verbose = verbose
+
         self._stepping = True
         self.name = 'marquee_' + self._font.__name__
+        self.pause(pause)
 
         # font details; only monochrome HLSB fonts are supported
         self._font_format = framebuf.MONO_HLSB
         self._font_colors = 2
         self._palette_format = framebuf.RGB565  # support up to 65536 colors when blitting
-
         # assemble color map
         self._palette_buf = bytearray(self._font_colors * 2)
         self._palette = framebuf.FrameBuffer(self._palette_buf, self._font_colors, 1, self._palette_format)
         self._palette.pixel(0, 0, self._bg)
         self._palette.pixel(self._font_colors -1, 0, self._fg)
-
+        # Work out how long our string will be once rendered
         self._stringwidth, self._height = self._line_size(self._string)
+        # Get width of target framebuffer
         try:
             self._width = self._device.width
         except:
             raise ValueError('{} does not appear to be a framebuffer'.format(self._device))
+        # padding size between repeats, in px
         self._padding = int(self._width * self._seperation)
+        # Give info as needed
         if verbose:
             print('{}:\n  string: {}'.format(self.name, self._string))
             print('  y: {}, height: {}'.format(self._y, self._height))
             print('  fg: {}, bg: {}, hgap: {}'.format(self._fg, self._bg, self._hgap), end='')
             print(', seperation: {}\n  pause: {}'.format(self._padding, self._pause),end='')
             print(', marquee width: {}, string width: {}'.format(self._width, self._stringwidth))
+        # determine if we need to animate at all (static marquee always re-drawn on step())
         if self._stringwidth <= self._width:
             self._stepping = False
             if verbose:
                 p = '{}: string width ({}) is smaller than screen width ({}), not animating'
                 print(p.format(self.name, self._stringwidth, self._width))
-
         # Make the framebuffer that we scroll
         self._sbwide = self._stringwidth + self._padding + self._width
         sbbytes = ((self._sbwide - 1) // 8 ) + 1
@@ -90,8 +93,10 @@ class ezFBmarquee():
         # write our string, add the seperator, then start the string again
         xpos = 0
         for c in self._string:
-            xpos += self._put_char(c,xpos) + self._hgap
+            xpos += self._put_char(c, xpos) + self._hgap
         xpos = xpos - self._hgap if xpos != 0 else xpos   # remove any trailing hgap
+        if not self._stepping:
+            return  # skip rollover space and chars if not animating
         xpos += self._padding
         for c in self._string:
             if xpos >= self._sbwide:
@@ -102,12 +107,19 @@ class ezFBmarquee():
         # fetch the glyph
         glyph, char_height, char_width = self._font.get_ch(char)
         if glyph is None:
-            return None, None  # Nothing to write
+            return 0  # Nothing to write
         # blit the glyph, make the background transparent..
         buf = bytearray(glyph)
         charbuf = framebuf.FrameBuffer(buf, char_width, char_height, self._font_format)
         self._scrollframe.blit(charbuf, x, 0, 0)
         return char_width
+
+    def pause(self, pause=None):
+        if pause:
+            self._pause = max(pause, -1)
+            if self._verbose:
+                print('pause added: {}'.format(self._pause))
+        return self._pause
 
     def step(self, step=1):
         # do animation step
@@ -122,7 +134,7 @@ class ezFBmarquee():
         # blit the output framebuffer to screen, we rely on the blit() for cropping
         # this is where colors are applied (via palette)
         self._device.blit(self._scrollframe, 0, self._y, -1, self._palette)
-        self._pause = self._pause -1 if self._pause > 0 else 0
+        self._pause = self._pause - 1 if self._pause > 0 else self._pause
         return roll
 
     def stop(self):
--- marquee/mdemo.py
+++ marquee/mdemo.py
@@ -1,22 +1,30 @@
 from repl_1306 import REPL_1306
 from ezFBmarquee import ezFBmarquee
-from sys import path
+from sys import path, argv
 from time import sleep
 
 # fonts
 path.append('fonts')
-import ezFBfont_10_courR08_ascii as font
+#import ezFBfont_17_helvB12_ascii as font
+import ezFBfont_23_spleen_12x24_ascii as font
+#import ezFBfont_16_open_iconic_all_2x_full as font
 
-message = 'Peachy'
+speed = float(argv[1]) if len(argv) == 2 else 1
 
-print('go')
-display = REPL_1306(80,12, zero=' ')
+message = 'This is quite a long 123 message, with %$@ stuff...'
 
-marquee = ezFBmarquee(display, font, 'Peachy Demo of marquee.', verbose=True)
-#marquee = ezFBmarquee(display, font, 'Peachy', verbose=True)
+w = 100
+h = 28
+
+display = REPL_1306(w, h, zero=' ', one ='█', clear=True)
+display.rect(0,0,w,h,1)
+
+marquee = ezFBmarquee(display, font, message, y=2, hgap=-1, pause=10, verbose=True)
 
 while True:
-    marquee.step()
-    #display.show()
-    sleep(0.2)
+    sleep(speed)
+    if marquee.step(3):
+        print('Rollover')
+        marquee.pause(10)
+    display.show()
 
