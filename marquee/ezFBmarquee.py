c4cd12903fb4016211dbb8afef1aac357d719403
Owen <owen@easytarget.org>
2024-06-29 23:50:41 +0200

    functionality

--- marquee/ezFBmarquee.py
+++ marquee/ezFBmarquee.py
@@ -7,57 +7,57 @@ import framebuf
 # Basic marquee class
 class ezFBmarquee():
 
-    def __init__(self,
-                 display,
-                 font,
-                 string,
-                 y = 0,
-                 seperation = 0.5,
-                 pause = 0,
-                 hgap = 0,
-                 fg = 1,
-                 bg = 0,
-                 verbose = False):
+    def __init__(self, display, font, y=0, hgap=0, fg=1, bg=0, verbose = False):
 
         self._device = display
         self._font = font
-        self._string = string.split('\n')[0]
         self._y = y
-        self._seperation = max(seperation,0)
+        self._hgap = hgap
         self._fg = fg
         self._bg = bg
-        self._hgap = hgap
         self._verbose = verbose
 
-        self._stepping = True
         self.name = 'marquee_' + self._font.__name__
-        self.pause(pause)
 
         # font details; only monochrome HLSB fonts are supported
         self._font_format = framebuf.MONO_HLSB
         self._font_colors = 2
         self._palette_format = framebuf.RGB565  # support up to 65536 colors when blitting
-        # assemble color map
+        # color map
         self._palette_buf = bytearray(self._font_colors * 2)
         self._palette = framebuf.FrameBuffer(self._palette_buf, self._font_colors, 1, self._palette_format)
-        self._palette.pixel(0, 0, self._bg)
-        self._palette.pixel(self._font_colors -1, 0, self._fg)
-        # Work out how long our string will be once rendered
-        self._stringwidth, self._height = self._line_size(self._string)
-        # Get width of target framebuffer
+        # Get height of marquee
+        self._height = self._font.height()
+        # Get width of marquee (full device width)
         try:
             self._width = self._device.width
         except:
             raise ValueError('{} does not appear to be a framebuffer'.format(self._device))
+        # Give info as needed
+        if self._verbose:
+            print('{}:'.format(self.name))
+            print('  y: {}, height: {}, width: {}'.format(self._y, self._height, self._width))
+            print('  fg: {}, bg: {}, hgap: {}'.format(self._fg, self._bg, self._hgap))
+ 
+    def _line_size(self, string):
+        x = 0
+        for char in string:
+            _, _, char_width = self._font.get_ch(char)
+            x += char_width + self._hgap if char_width > 0 else 0
+        x = x - self._hgap if x != 0 else x   # remove any trailing hgap
+        return x, self._font.height()
+
+    def _makescroll(self):
+        self._stepping = True
+        # Work out how long our string will be once rendered
+        self._stringwidth, _ = self._line_size(self._string)
         # padding size between repeats, in px
         self._padding = int(self._width * self._seperation)
         # Give info as needed
         if self._verbose:
-            print('{}:\n  string: {}'.format(self.name, self._string))
-            print('  y: {}, height: {}'.format(self._y, self._height))
-            print('  fg: {}, bg: {}, hgap: {}'.format(self._fg, self._bg, self._hgap), end='')
-            print(', seperation: {}\n  pause: {}'.format(self._padding, self._pause),end='')
-            print(', marquee width: {}, string width: {}'.format(self._width, self._stringwidth))
+            print('{}:\n  string: {}\n  string width: {}'.format(self.name, self._string, self._stringwidth))
+            print('  seperation: {}\n  pause: {}'.format(self._padding, self._pause))
+            print('  fg: {}, bg: {}, hgap: {}'.format(self._fg, self._bg, self._hgap))
         # determine if we need to animate at all (static marquee always re-drawn on step())
         if self._stringwidth <= self._width:
             self._stepping = False
@@ -69,20 +69,7 @@ class ezFBmarquee():
         sbbytes = ((self._sbwide - 1) // 8 ) + 1
         self._scrollbuf =  bytearray(sbbytes * self._height)
         self._scrollframe = framebuf.FrameBuffer(self._scrollbuf, self._sbwide, self._height, self._font_format)
-        # Fill for the first time
-        self._fillscroll()
-        # Show the initial output
-        self.step(0) 
- 
-    # Functions
 
-    def _line_size(self, string):
-        x = 0
-        for char in string:
-            _, _, char_width = self._font.get_ch(char)
-            x += char_width + self._hgap if char_width > 0 else 0
-        x = x - self._hgap if x != 0 else x   # remove any trailing hgap
-        return x, self._font.height()
 
     def _fillscroll(self):
         # clean the scrollbuffer
@@ -112,14 +99,43 @@ class ezFBmarquee():
         self._scrollframe.blit(charbuf, x, 0, 0)
         return char_width
 
-    def pause(self, pause=None):
-        if pause is not None:
-            self._pause = max(pause, -1)
-            if self._verbose:
-                print('pause added: {}'.format(self._pause))
-        return self._pause
+    def _stop(self, clean=True):
+        self._string = ''
+        self._stepcount = 0
+        self._stepping = False
+        self._stringwidth = 0
+        self._padding = 0
+        self._sbwide = 0
+        del self._scrollframe, self._scrollbuf
+        if clean:
+            self._device.rect(0, self._y, self._width, self._height, self._bg, True)
+
+    def start(self, string, seperation=0.5, pause=0, hgap=None, fg=None, bg=None):
+        # start marquee with string, returns false if no need to animate.
+        self._string = self._string if string is None else string.split('\n')[0]
+        self._seperation = max(seperation,0)
+        self._hgap = self._hgap if hgap is None else hgap
+        self._fg = self._fg if fg is None else fg
+        self._bg = self._bg if bg is None else bg
+        if len(self._string) == 0:
+            self._stop()
+            return False
+        # Initial pause
+        self.pause(pause)
+        # set color map
+        self._palette.pixel(0, 0, self._bg)
+        self._palette.pixel(self._font_colors -1, 0, self._fg)
+        # Create the scroll buffer
+        self._makescroll()
+        # Fill for the first time
+        self._fillscroll()
+        # Show the initial output
+        self.step(0) 
+        return self._stepping
 
     def step(self, step=1):
+        if len(self._string) == 0:
+            return False
         # do animation step
         roll = False
         if self._stepping and (self._pause == 0):
@@ -135,12 +151,9 @@ class ezFBmarquee():
         self._pause = self._pause - 1 if self._pause > 0 else self._pause
         return roll
 
-    def stop(self, clean=True):
-        # cleanup and delete buffers etc
-        if clean:
-            self._device.rect(0, self._y, self._width, self._height, self._bg, True)
-        del self._scrollframe, self._scrollbuf
-        return
-
-    # Owen: TEST:
-    #             text shorter then width.. is whole area stil filled?
+    def pause(self, pause=None):
+        if pause is not None:
+            self._pause = max(pause, -1)
+            if self._verbose:
+                print('pause: {}'.format(self._pause))
+        return self._pause
--- marquee/mdemo.py
+++ marquee/mdemo.py
@@ -5,26 +5,31 @@ from time import sleep
 
 # fonts
 path.append('fonts')
-#import ezFBfont_17_helvB12_ascii as font
-import ezFBfont_23_spleen_12x24_ascii as font
+import ezFBfont_17_helvB12_ascii as font
+#import ezFBfont_23_spleen_12x24_ascii as font
 #import ezFBfont_16_open_iconic_all_2x_full as font
 
 speed = float(argv[1]) if len(argv) == 2 else 1
 
 message = 'This is quite a long 123 message, with %$@ stuff...'
 
-w = 100
-h = 28
+w = 80
+h = 18
 
-display = REPL_1306(w, h, zero=' ', one ='█', clear=True)
-display.rect(0,0,w,h,1)
+display = REPL_1306(w, h, zero='.', one ='█', clear=True)
 
-marquee = ezFBmarquee(display, font, message, y=2, hgap=-1, pause=10, verbose=True)
+marquee = ezFBmarquee(display, font, verbose=True)
 
+marquee.start(message, pause=100)
+
+i = True
 while True:
     sleep(speed)
-    if marquee.step(3):
-        print('Rollover')
-        marquee.pause(10)
+    if marquee.step(1):
+        if i:
+            marquee.start('This is not so long', fg=1, bg=0, pause=50)
+        else:
+            marquee.start('Short', fg=0, bg=1)
+        i = not i
     display.show()
 
