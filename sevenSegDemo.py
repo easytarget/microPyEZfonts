48bad97f37c6de7e37317645b5c40c22c9b9e05b
Owen <owen@easytarget.org>
2026-03-11 18:23:34 +0100

    update demo to suit new class

--- sevenSegDemo.py
+++ sevenSegDemo.py
@@ -5,23 +5,26 @@ path.append('drivers')
 from repl_1306 import REPL_1306
 path.append('demo_extra')
 from ezFBfont import ezFBfont
-import ezFBsevenSeg as sevenSeg
+from ezFBsevenSeg import SEVEN_SEG
 
 # Display
-display = REPL_1306(132, 102, clear=False, blocks=True)
+display = REPL_1306(100, 64, clear=False, blocks=True)
 display.invert(False)  # as needed
 display.rotate(0)      # as needed
 display.contrast(128)  # as needed
 
-bigtime = ezFBfont(display, sevenSeg)
+# Create a font instance
+bigtimefont = SEVEN_SEG()
 
-sevenSeg.set(pre='0123')
+# Set it up
+bigtimefont.set(height=58, width=32, thick=5, gap=3)
 
-print(sevenSeg.info())
+# Now create a font writer
+bigtime = ezFBfont(display, bigtimefont)
 
-#sevenSeg.conv()
-bigtime.write('-.:',0,0)
-bigtime.write('01234567\n89ABCDEF',0,34)
+#bigtime.write(' -\u2009.\u00B7\u02D9:\n01234567\n89ABCDEF',0,0)
+bigtime.write('8:2',0,0)
 
 display.show()
-print(sevenSeg.info())
+print(' -\u2009.\u00B7\u02D9:\n01234567\n89ABCDEF')
+print(bigtimefont.info())
