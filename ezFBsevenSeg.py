188dd67e8dde1ff2d1e65cea8ef7878d038ba465
Owen <owen@easytarget.org>
2026-03-13 19:43:38 +0100

    sensible defuaults, under/overscore

--- ezFBsevenSeg.py
+++ ezFBsevenSeg.py
@@ -42,6 +42,8 @@ _CHARS_FULL = {
     68 : ['bm','bb','ll','rl','ru'],            # D
     69 : ['bt','bm','bb','lu','ll'],            # E
     70 : ['bt','bm','lu','ll'],                 # F
+    95 : ['bb'],                                # underscore: '_'
+    175 : ['bt'],                               # macron (overscore): '¯'
 }
 
 _CHARS_HALF = {
@@ -62,15 +64,22 @@ class SEVEN_SEG:
     weight = 'medium'
     size = 32   # default
 
-    def __init__(self, height, width,
-                 led_high=None, led_wide=None,
-                 led_thick=3, led_gap=3,
+    def __init__(self, width, height,
+                 led_wide=None, led_high=None,
+                 led_thick=None, led_gap=None,
                  use_cache=True, precache=None):
+        # needed by ez font writer
         self.__name__ = '{}-{}x{}'.format(self.__module__, height, width)
+        # constrain and set width and height
+        self._wide = max(4, width)
+        self._high = max(6, height)
         # Sensible (?) defaults
-        led_high = int(height * 0.8) if led_high is None else led_high
-        led_wide = int(width * 0.8) if led_wide is None else led_wide
-        self.set(height, width, led_high, led_wide, led_thick, led_gap, use_cache, precache)
+        led_wide = self._wide - 1 - (self._wide // 8) if led_wide is None else led_wide
+        led_high = self._high - 1 - (self._high // 8) if led_high is None else led_high
+        led_thick = 1 + floor(width // 10) if led_thick is None else led_thick
+        led_gap = 1 + floor(width // 20) if led_gap is None else led_gap
+        # Apply initial settings
+        self.set(led_wide, led_high, led_thick, led_gap, use_cache, precache)
 
     '''
         Internal methods specific to the Seven Segment font
@@ -97,8 +106,8 @@ class SEVEN_SEG:
 
     def _render_half(self, segments):
         # Render the char using a half-width framebuf, returns a bytearray
-        wide = ceil(self._wide/2)
-        led_wide = ceil(self._led_wide/2)
+        wide = ceil(self._wide / 2)
+        led_wide = ceil(self._led_wide / 2)
         bytewide = ((wide - 1) // 8) + 1
         buf = bytearray(self._high * bytewide)
         canvas = FrameBuffer(buf, wide, self._high, MONO_HLSB)
@@ -115,7 +124,8 @@ class SEVEN_SEG:
             Draw a Full-Width character using the 7 led segments
         '''
         # Draw the required bars in full (intersecting)
-        M = int(Y/2)
+        M = round(Y/2)
+        R = Y - M
         for l in range(T):
             if 'bt' in elements:
                 canvas.hline(l, l, X-(2*l), 1)
@@ -126,9 +136,9 @@ class SEVEN_SEG:
             if 'bm' in elements:
                 canvas.hline(l, M+l, X-(2*l), 1)
             if 'll' in elements:
-                canvas.vline(l, M+l, M-(2*l), 1)
+                canvas.vline(l, M+l, R-(2*l), 1)
             if 'rl' in elements:
-                canvas.vline(X-l-1,  M+l, M-(2*l), 1)
+                canvas.vline(X-l-1,  M+l, R-(2*l), 1)
             if 'bb' in elements:
                 canvas.hline(l, Y-l-1, X-(2*l), 1)
         # Now create the gaps that seperate them
@@ -143,11 +153,12 @@ class SEVEN_SEG:
             canvas.line(X - x - 1, Y - y - 1, X - T - x - 1, Y - T - y - 1, 0)
 
     def _draw_half(self, canvas, X, Y, T, elements):
-        U = int(Y*0.35)
-        M = int(Y/2)
-        L = int(Y*0.68)
-        C = int(X/2)
-        i = int(T/2)
+        U = floor(Y*0.33)
+        M = round(Y/2)
+        L = round(Y*0.66)
+        C = X // 2
+        i = T // 2
+        #print('X Y U M L C T i :: ', X, Y, U, M, L, C, T, i)
         if 'de' in elements:
             canvas.rect(C-i, Y-T, T, T, 1, True)
         if 'dm' in elements:
@@ -188,8 +199,7 @@ class SEVEN_SEG:
         return max(_ALL_CHARS)
 
     # NEEDS HEAVY RE_WRITE
-    def set(self, height=None, width=None,
-                  led_high=None, led_wide=None,
+    def set(self, led_wide=None, led_high=None,
                   led_thick=None, led_gap=None,
                   cached=None, precache=None):
         '''
@@ -198,34 +208,30 @@ class SEVEN_SEG:
             -   All parameters are optional
             -   Always clears the cache
         '''
-        # Always clean cache
-        self._cache = {}
-        # constrain
-        # TODO
         # set new value as necesscary
-        self._high = height if height is not None else self._high
-        self._wide = width if width is not None else self._wide
-        self._led_high = led_high if led_high is not None else self._led_high
         self._led_wide = led_wide if led_wide is not None else self._led_wide
+        self._led_high = led_high if led_high is not None else self._led_high
         self._led_thick = led_thick if led_thick is not None else self._led_thick
         self._led_gap = led_gap if led_gap is not None else self._led_gap
         self._use_cache = cached if cached is not None else self._use_cache
+        # constrain
+        self._led_wide = max(3, min(self._led_wide, self._wide))
+        self._led_high = max(5, min(self._led_high, self._high))
+        self._led_thick = max(1, self._led_thick)
         # Calculate led offset within char
         self._addx = floor((self._wide - self._led_wide) / 2)
         self._addy = floor((self._high - self._led_high) / 2)
+        # Always clean cache
+        self._cache = {}
         # Pre-cache any chars passed by 'pre'
-        # - this works even if caching is disabled
         if precache is not None:
             for ch in precache:
                 _, _, _ = self.get_ch(ch)
 
     # DEBUG: remove this later..
     def info(self):
-        # useful for debug; returns height, width
-        # cache active(bool),and any current chached chars as a list
-        c = list(self._cache.keys())
-        c.sort()
-        return self._wide, self._high, self._led_thick, self._led_gap, self._use_cache, c
+        print('Width: {}, Height: {},\nled_wide: {}, led_high: {},\nled_thick: {}, led_gap: {},\naddx: {}, addy: {}'
+              .format(self._wide, self._high, self._led_wide, self._led_high, self._led_thick, self._led_gap, self._addx, self._addy))
 
     '''
         get_ch() returns the glyph data
--- sevenSegDemo.py
+++ sevenSegDemo.py
@@ -8,24 +8,26 @@ from ezFBfont import ezFBfont
 from ezFBsevenSeg import SEVEN_SEG
 
 X = 24
-Y = 36
+Y = 34
 
 # Display
-display = REPL_1306(X * 5, Y * 1, clear=False, blocks=True)
+display = REPL_1306(X * 5, Y * 2, clear=False, blocks=True)
+display.fill(1)
 
 # Create a font instance
-bigtimefont = SEVEN_SEG(height = Y, width = X, led_thick = 3)
+bigtimefont = SEVEN_SEG(height = Y, width = X)
 
 # Set font up
-bigtimefont.set(led_high=32, led_thick=4, led_gap=2)
+#bigtimefont.set()
 
 # Now create a font writer
-bigtime = ezFBfont(display, bigtimefont, hgap=1, vgap=1, fg=0, bg=1)
+bigtime = ezFBfont(display, bigtimefont, hgap=1, vgap=1, fg=1, bg=0)
 
 #bigtime.write(' -\u2009.\u00B7\u02D9:\n01234567\n89ABCDEF',0,0)
 #bigtime.write('08:34\n56.79\u00B721\n ABCDEF',0,0)
-bigtime.write('08:34',0,0)
+bigtime.write('08:34\n¯-_.\u00B7\u02D9',0,0)
+#bigtime.write(':8',0,0)
 
 display.show()
 #print(' -\u2009.\u00B7\u02D9:\n01234567\n89ABCDEF')
-print(bigtimefont.info())
+bigtimefont.info()
